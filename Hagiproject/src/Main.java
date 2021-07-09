09d79e864f56d0d5b4dcb2019d71c74502dd0198
s-tonda <s-tonda@LAPTOP-3B41UTN8>
2021-07-09 09:51:50 +0900

    tonda変更

--- Hagiproject/src/Main.java
+++ Hagiproject/src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("萩原義人sfytfu");
+		System.out.println("萩原義人");
+		System.out.println("tonda");
 	}
 
 }
