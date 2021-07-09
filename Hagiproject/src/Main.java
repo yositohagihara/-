b565bda9cd20d7ecba3f37af01600277d7bee77a
s-tonda <s-tonda@LAPTOP-3B41UTN8>
2021-07-09 10:34:04 +0900

    自分の名前

--- Hagiproject/src/Main.java
+++ Hagiproject/src/Main.java
@@ -3,8 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("萩原");
-		System.out.println("tonda");
+
+		System.out.println("tondasyusaku");
 	}
 
 
